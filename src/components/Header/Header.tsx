c368bd1c102d8186e616a22a5c44fd16e250eb2d
Sapper <saniamykhailenko@gmail.com>
2026-04-06 12:36:45 +0300

    upd header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,13 +1,25 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import css from "./Header.module.css";
+import clsx from "clsx";
 
 export default function Header() {
+  const location = useLocation();
+
   return (
     <header className={css.header}>
-      <Link to="/" className={css.link}>
+      <Link
+        to="/"
+        className={clsx(css.link, location.pathname === "/" && css.currentPage)}
+      >
         Main
       </Link>
-      <Link to="/variables" className={css.link}>
+      <Link
+        to="/variables"
+        className={clsx(
+          css.link,
+          location.pathname === "/variables" && css.currentPage,
+        )}
+      >
         Variables
       </Link>
     </header>
